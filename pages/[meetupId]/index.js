7208f3e258e43632d90f8a410e73a227660283d3
Abraham <nigussieabreham80@gmail.com>
2023-07-03 10:56:26 +0300

    Adding new-meetup page and meetup detail(dynamic)

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetailPage() {
+  return (
+    <MeetupDetail
+      title="first meetup"
+      image="https://officialpsds.com/imageview/7q/pj/7qpj3w_large.png?1550944546"
+      address="some street, city."
+      description="This the first meetup"
+    />
+  );
+}
+
+export default MeetupDetailPage;
